9be6e2962a99bd7bfc48a8ff25fb7087c7a64fbd
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-10 13:59:32 +0200

    Apply eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base', 'prettier',
+    'airbnb-base',
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
   },
   rules: {
   },
--- gameOfLife.js
+++ gameOfLife.js
@@ -2,53 +2,50 @@
 const numberOfRows = 10;
 const numberOfColumns = 13;
 
-
-// initialize current state and next state matrices and fill them with zeros 
-let matrix = new Array(numberOfRows).fill(0).map( () => new Array(numberOfColumns).fill(0) );
-let nextMatrix = new Array(numberOfRows).fill(0).map( () => new Array(numberOfColumns).fill(0) );
-
+// initialize current state and next state matrices and fill them with zeros
+let matrix = new Array(numberOfRows).fill(0).map(() => new Array(numberOfColumns).fill(0));
+const nextMatrix = new Array(numberOfRows).fill(0).map(() => new Array(numberOfColumns).fill(0));
 
 // we create the matrix of divs that will be painted in the HTML file
 function createMatrixHTML() {
-  let world = document.querySelector(".world");
-  let matrixHTML = document.createElement("table");
-  matrixHTML.className = "matrixHTML";
+  const world = document.querySelector('.world');
+  const matrixHTML = document.createElement('table');
+  matrixHTML.className = 'matrixHTML';
 
-  for (let i=0; i < numberOfRows; i++) {
-    let tr = document.createElement("tr");
-    
-    for (let j=0; j< numberOfColumns; j++) {
-      let cell = document.createElement("td");
-      let textToCell = document.createTextNode(0);
+  for (let i = 0; i < numberOfRows; i++) {
+    const tr = document.createElement('tr');
+
+    for (let j = 0; j < numberOfColumns; j++) {
+      const cell = document.createElement('td');
+      const textToCell = document.createTextNode(0);
       cell.appendChild(textToCell);
 
-      cell.className = "dead";
-      cell.setAttribute("id", i + "_" + j);
+      cell.className = 'dead';
+      cell.setAttribute('id', `${i}_${j}`);
       cell.onclick = cellClick;
       tr.appendChild(cell);
     }
     matrixHTML.appendChild(tr);
   }
   world.appendChild(matrixHTML);
-
-}  
+}
 
 function cellClick() {
-  // the user clicks on the cell to enter the initial pattern and 
+  // the user clicks on the cell to enter the initial pattern and
   // mutate the initial matrix of dead cells
-  this.innerHTML = "";
-  let newValue = document.createTextNode(1);
-  this.appendChild(newValue); 
-  this.className = "alive"; 
-  let [row, column] =this.id.split("_").map(x=> +x);
+  this.innerHTML = '';
+  const newValue = document.createTextNode(1);
+  this.appendChild(newValue);
+  this.className = 'alive';
+  const [row, column] = this.id.split('_').map((x) => +x);
   matrix[row][column] = 1;
 }
 
 /* define pattern generation */
 
 // count the neighbours
-function countNeighbours(row,columna) {
-   let count = 0;
+function countNeighbours(row, columna) {
+  let count = 0;
 
   if (row === 0) {
     count = countOnFirstRow();
@@ -57,9 +54,8 @@ function countNeighbours(row,columna) {
   } else {
     count = countOnMiddleRow();
   }
-  
-  return count; 
 
+  return count;
 
   function countOnFirstRow() {
     if (columna === 0) {
@@ -75,7 +71,7 @@ function countNeighbours(row,columna) {
       count += matrix[row][columna - 1];
       count += matrix[row][columna + 1];
 
-      // 2. One row down 
+      // 2. One row down
       count += matrix[row + 1][columna - 1];
       count += matrix[row + 1][columna];
       count += matrix[row + 1][columna + 1];
@@ -98,7 +94,7 @@ function countNeighbours(row,columna) {
       count += matrix[row][columna - 1];
       count += matrix[row][columna + 1];
 
-      // 2. One row down 
+      // 2. One row down
       count += matrix[row - 1][columna - 1];
       count += matrix[row - 1][columna];
       count += matrix[row - 1][columna + 1];
@@ -125,12 +121,12 @@ function countNeighbours(row,columna) {
       count += matrix[row][columna - 1];
       count += matrix[row][columna + 1];
 
-      // 2. One row down 
+      // 2. One row down
       count += matrix[row - 1][columna - 1];
       count += matrix[row - 1][columna];
       count += matrix[row - 1][columna + 1];
 
-      // 2. One row up 
+      // 2. One row up
       count += matrix[row + 1][columna - 1];
       count += matrix[row + 1][columna];
       count += matrix[row + 1][columna + 1];
@@ -140,16 +136,15 @@ function countNeighbours(row,columna) {
   }
 }
 
-
 // define the rules that take us to the next state matrix
 function transition() {
   console.log(matrix);
   for (let row = 0; row < numberOfRows; row++) {
-    for (let col = 0;  col < numberOfColumns; col++) {
-      let numberOfNeighbours = countNeighbours(row,col);
-      // apply the rules of the game  
+    for (let col = 0; col < numberOfColumns; col++) {
+      const numberOfNeighbours = countNeighbours(row, col);
+      // apply the rules of the game
       // if alive, stays alive if it has min 2 neighbours alive; otherwise it dies
-      if (matrix[row][col]=== 1) {
+      if (matrix[row][col] === 1) {
         if (numberOfNeighbours < 2) {
           nextMatrix[row][col] = 0;
         } else if (numberOfNeighbours === 2 || numberOfNeighbours === 3) {
@@ -157,39 +152,37 @@ function transition() {
         } else if (numberOfNeighbours > 3) {
           nextMatrix[row][col] = 0;
         }
-      } else if (matrix[row][col]===0) {
+      } else if (matrix[row][col] === 0) {
         // if dead, it becomes alive if it has exactly 3 neighbours alive
-        if (numberOfNeighbours===3) {
+        if (numberOfNeighbours === 3) {
           nextMatrix[row][col] = 1;
         } else {
           nextMatrix[row][col] = 0;
         }
       }
-    }  
-  } 
+    }
+  }
   console.log(nextMatrix);
   matrix = nextMatrix;
 }
 
 // update the world
 function updateWorld() {
-  let cell='';
+  let cell = '';
   for (let row = 0; row < numberOfRows; row++) {
-    for (let col = 0;  col < numberOfColumns; col++) {
-            cell = document.getElementById(row + '_' + col);
-            if (nextMatrix[row][col] == 0) {
-                cell.setAttribute('class', 'dead');
-                cell.innerHTML = "";
-                let newValue = document.createTextNode(0);
-                cell.appendChild(newValue); 
-            } else {
-                cell.setAttribute('class', 'alive');
-                cell.innerHTML = "";
-                let newValue = document.createTextNode(1);
-                cell.appendChild(newValue); 
-            }
+    for (let col = 0; col < numberOfColumns; col++) {
+      cell = document.getElementById(`${row}_${col}`);
+      if (nextMatrix[row][col] == 0) {
+        cell.setAttribute('class', 'dead');
+        cell.innerHTML = '';
+        const newValue = document.createTextNode(0);
+        cell.appendChild(newValue);
+      } else {
+        cell.setAttribute('class', 'alive');
+        cell.innerHTML = '';
+        const newValue = document.createTextNode(1);
+        cell.appendChild(newValue);
+      }
     }
   }
 }
-
-
